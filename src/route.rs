c611b5c1632152d6a5bfaed9c6e15bdddb69789d
BezPowell <bez.powell@protonmail.com>
2020-08-12 09:57:36 +0100

    Document Route struct

--- src/route.rs
+++ src/route.rs
@@ -2,6 +2,8 @@ use std::convert::From;
 
 use movingai::Coords2D;
 
+///Describes a route between to points.
+///Giving the total distance needed to travel and a vector of each step needed.
 pub struct Route {
     distance: f64,
     steps: Vec<Coords2D>,
@@ -17,10 +19,13 @@ impl From<(f64, Vec<Coords2D>)> for Route {
 }
 
 impl Route {
+    ///Returns a vector of Coords2D, each representing a step in the path.
+    ///Organised in reverse order (destination is at [0]) to allow calling .pop() to get each step.
     pub fn steps(&self) -> Vec<Coords2D> {
         self.steps.clone()
     }
 
+    ///Returns the total distance needed to travel as an f64.
     pub fn distance(&self) -> f64 {
         self.distance
     }
