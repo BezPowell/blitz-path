9139609465fa56c48436e95c12204624e6988a3f
BezPowell <bez.powell@protonmail.com>
2020-08-12 10:16:56 +0100

    Add quick tests
    This splits up the integration tests into a quick and full test for each algorithm. By default only the quick tests will be run to avoid A* taking forever.

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,23 +4,30 @@ mod tests {
 
     use movingai::parser::parse_map_file;
     use movingai::parser::parse_scen_file;
+    use movingai::{MovingAiMap, SceneRecord};
 
     use blitz_path::{a_star_path, jps_path};
 
     const MAP: &str = "./tests/map/maze512-32-9.map";
     const SCEN: &str = "./tests/map/maze512-32-9.map.scen";
+    const TEST_NUMS_JPS: [usize; 3] = [0, 34, 1740];
+    const TEST_NUMS_A_STAR: [usize; 2] = [0, 34];
 
     enum Algorithm {
         AStar,
         JPS,
     }
 
-    fn test_scen(algorithm: Algorithm, map: &str, scen: &str) -> Vec<String> {
+    fn test_scen(
+        algorithm: Algorithm,
+        map: &MovingAiMap,
+        scen: &Vec<SceneRecord>,
+        tests: Vec<usize>,
+    ) -> Vec<String> {
         let mut errors = Vec::new();
-        let map = parse_map_file(Path::new(map)).unwrap();
-        let scenes = parse_scen_file(Path::new(scen)).unwrap();
 
-        for (i, scene) in scenes.iter().enumerate() {
+        for index in tests {
+            let scene = &scen[index];
             let path = match algorithm {
                 Algorithm::AStar => a_star_path(&map, scene.start_pos, scene.goal_pos),
                 Algorithm::JPS => jps_path(&map, scene.start_pos, scene.goal_pos),
@@ -28,8 +35,7 @@ mod tests {
 
             match path {
                 None => {
-                    //panic!("Test failed at scen #{}", i);
-                    errors.push(format!("Test #{} found no path", i));
+                    errors.push(format!("Test #{} found no path", index));
                 }
                 Some(path) => {
                     for num in 0..path.steps().len() - 1 {
@@ -43,7 +49,7 @@ mod tests {
                             || direction_y < -1
                             || direction_y > 1
                         {
-                            errors.push(format!("Test #{} did not unwind correctly", i))
+                            errors.push(format!("Test #{} did not unwind correctly", index))
                         }
                     }
                 }
@@ -55,7 +61,27 @@ mod tests {
 
     #[test]
     fn jps() {
-        let errors = test_scen(Algorithm::JPS, MAP, SCEN);
+        let map = parse_map_file(Path::new(MAP)).unwrap();
+        let scenes = parse_scen_file(Path::new(SCEN)).unwrap();
+        let tests = TEST_NUMS_JPS.to_vec();
+
+        let errors = test_scen(Algorithm::JPS, &map, &scenes, tests);
+
+        assert!(
+            errors.len() < 1,
+            "The following tests failed:\n{:?}",
+            errors
+        );
+    }
+
+    #[test]
+    #[ignore = "very slow"]
+    fn jps_full() {
+        let map = parse_map_file(Path::new(MAP)).unwrap();
+        let scenes = parse_scen_file(Path::new(SCEN)).unwrap();
+        let all_tests = (0..scenes.len()).collect();
+
+        let errors = test_scen(Algorithm::JPS, &map, &scenes, all_tests);
 
         assert!(
             errors.len() < 1,
@@ -66,7 +92,27 @@ mod tests {
 
     #[test]
     fn a_star() {
-        let errors = test_scen(Algorithm::AStar, MAP, SCEN);
+        let map = parse_map_file(Path::new(MAP)).unwrap();
+        let scenes = parse_scen_file(Path::new(SCEN)).unwrap();
+        let tests = TEST_NUMS_A_STAR.to_vec();
+
+        let errors = test_scen(Algorithm::AStar, &map, &scenes, tests);
+
+        assert!(
+            errors.len() < 1,
+            "The following tests failed:\n{:?}",
+            errors
+        );
+    }
+
+    #[test]
+    #[ignore = "extremely slow"]
+    fn a_star_full() {
+        let map = parse_map_file(Path::new(MAP)).unwrap();
+        let scenes = parse_scen_file(Path::new(SCEN)).unwrap();
+        let all_tests = (0..scenes.len()).collect();
+
+        let errors = test_scen(Algorithm::AStar, &map, &scenes, all_tests);
 
         assert!(
             errors.len() < 1,
