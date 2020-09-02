1300d9381857fc1b112d70dc686e86353c4b9605
BezPowell <bez.powell@protonmail.com>
2020-09-02 13:59:53 +0100

    Add note on compiling.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,12 @@
 //! `blitz-path` contains (hopefully) lightning-quick implementations of various pathfinding algorithms. Currently in a very wip state. It relies on the [movingai-rust](https://github.com/THeK3nger/movingai-rust) crate for map implementation and testing / benchmarks.
 //!
 //! It currently provides implementations of the A* and JPS pathfinding algorithms.
+//!
+//! *A note on compiling:* Compiling the crate with "fat" LTO can greatly improve performance. However, it also substantially slows down compilation, so it is only recommended to use this when building for release. To enable fat LTO for the `--release` flag add the following to your project's `cargo.toml` file.
+//! ```
+//! [profile.release]
+//! lto = "fat"
+//! ```
 
 mod astar;
 mod jps;
